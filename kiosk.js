28b3c37fdc6f48250e477ba3c5487ac1679fe319
Odoyo-Queenter <achiengodoyoqueenter@gmail.com>
2022-08-24 09:08:03 +0300

    fetch

--- kiosk.js
+++ kiosk.js
@@ -1,49 +1,50 @@
 fetch('http://localhost:5000/products')
-.then(function(response) {
-    return response.json();
-})
-.then(function(info) {
- console.log(info);
-let myProducts = document.getElementById('products')
-info.forEach(i => {
-    let ItemList = document.createElement('li')
-    ItemList.innerText = `${i.name}`
-    myProducts.appendChild(ItemList)
-})
-})
-.catch(function(error){
-    console.log('error',error);
-   });
-   fetch('http://localhost:5000/products/vegetables')
-   .then(function(response) {
-       return response.json();
-   })
-   .then(function(info) {
-    console.log(info);
-   let myProducts = document.getElementById('products')
-   info.forEach(i => {
-       let ItemList = document.createElement('li')
-       ItemList.innerText = `${i.name}`
-       myProducts.appendChild(ItemList)
-   })
-   })
-   .catch(function(error){
-    console.log('error',error);
-   })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (pro) {
+        console.log(pro);
 
-   .catch(function(error){
- console.log('error',error);
-});
+        let myProducts = document.getElementById('products')
+        pro.forEach(i => {
+            let ItemList = document.createElement('li')
+            ItemList.innerText = `${i.name}`
+            myProducts.appendChild(ItemList)
+        })
+    })
+    .catch(function (error) {
+        console.log('error', error);
+    });
+fetch('http://localhost:5000/products/vegetables')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (info) {
+        console.log(info);
+        let myProducts = document.getElementById('fruList')
+        info.forEach(i => {
+            let ItemList = document.createElement('li')
+            ItemList.innerText = `${i.name}`
+            myProducts.appendChild(ItemList)
+        })
+    })
+    .catch(function (error) {
+        console.log('error', error);
+    })
+
+    .catch(function (error) {
+        console.log('error', error);
+    });
 fetch('http://localhost:5000/products/Fruits')
-.then(function(response) {
-    return response.json();
-})
-.then(function(info) {
- console.log(info);
-let myProducts = document.getElementById('products')
-info.forEach(i => {
-    let ItemList = document.createElement('li')
-    ItemList.innerText = `${i.name}`
-    myProducts.appendChild(ItemList)
-})
-});
+    .then(function (getResponse) {
+        return getResponse.json();
+    })
+    .then(function (info) {
+        console.log(info);
+        let myProducts = document.getElementById('vegList')
+        info.forEach(i => {
+            let ItemList = document.createElement('li')
+            ItemList.innerText = `${i.name}`
+            myProducts.appendChild(ItemList)
+        })
+    });
